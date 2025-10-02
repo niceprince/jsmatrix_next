eb9765fbf48e06a83c24590c103d3e2847ea95a3
Prince <prince@Princes-MacBook-Pro.local>
2025-10-02 07:43:12 +0530

    added gtag script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Exo } from "next/font/google";
 import Header from "@/components/header/Header";
 import "./globals.css";
@@ -16,9 +17,12 @@ const exoSans = Exo({
 // });
 
 export const metadata: Metadata = {
-  title: "JS Matrix | Web Development, Software & Design and Printing Solutions",
-  keywords: "Web Development, Web Maintanance, Web Designing, Graphic Designing, Printing Designing",
-  description: "Expert services in web development, website design, software development, SEO, and web page performance. We also provide emailer design, graphic designs, book and card designs, banner designs, printing solutions, poster and book printing, stationery, and computer hardware services.",
+  title:
+    "JS Matrix | Web Development, Software & Design and Printing Solutions",
+  keywords:
+    "Web Development, Web Maintanance, Web Designing, Graphic Designing, Printing Designing",
+  description:
+    "Expert services in web development, website design, software development, SEO, and web page performance. We also provide emailer design, graphic designs, book and card designs, banner designs, printing solutions, poster and book printing, stationery, and computer hardware services.",
   icons: {
     icon: "/favicon.svg", // relative to /public
   },
@@ -33,10 +37,20 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.svg" sizes="any" type="image/svg+xml" />
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-PN4B9822T7"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-PN4B9822T7');
+          `}
+        </Script>
       </head>
-      <body
-        className={`${exoSans.variable} antialiased`}
-      >
+      <body className={`${exoSans.variable} antialiased`}>
         <ScrollToTop />
         <Header />
         {children}
